ab864e62979db1dbfed27b87e3c857e86f5ca3d1
Rin <87538369@qq.com>
2021-04-26 00:35:49 +0800

    完成订单页面

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -24,7 +24,8 @@ import {
   Tabs, TabPane,
   Steps, Step,
   Checkbox, CheckboxGroup,
-  Upload
+  Upload,
+  Timeline, TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -65,6 +66,8 @@ Vue.use(Step)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
